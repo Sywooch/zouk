e800d03b000af221e8861cd091341bf9f73a9b70
IVsevolod <v.ivanov@zavento.com>
2017-01-22 13:00:43 +0300

    Версия css и js скриптов

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,7 +18,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/site.css?20170122',
     ];
     public $js = [
     ];
