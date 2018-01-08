d3550b2b8bbd9c11e4c79dac38cc287f5dee5d40
IVsevolod <v.ivanov@zavento.com>
2018-01-08 09:40:42 +0300

    Добавлены команды в бот телеграм.

--- frontend/controllers/TelegramController.php
+++ frontend/controllers/TelegramController.php
@@ -3,6 +3,7 @@
 namespace frontend\controllers;
 
 use common\components\TelegramBotComponent;
+use common\models\Video;
 use TelegramBot\Api\Types\Message;
 use Yii;
 use yii\filters\AccessControl;
@@ -32,6 +33,15 @@ class TelegramController extends Controller
         ];
     }
 
+    public function beforeAction($action)
+    {
+        if (in_array($action->id, ['init'])) {
+            $this->enableCsrfValidation = false;
+        }
+
+        return parent::beforeAction($action);
+    }
+
 
     public function actionInit()
     {
@@ -44,7 +54,20 @@ class TelegramController extends Controller
             $telegramBot->sendMessage($message->getChat()->getId(), $answer);
         });
 
-        $bot->run();
+        $bot->command('help',  function (Message $message) use ($bot, $telegramBot) {
+            $answer = 'Привет! Добро пожаловать в ProZouk бот.';
+            $telegramBot->sendMessage($message->getChat()->getId(), $answer);
+        });
+
+
+        $bot->command('randomVideo',  function (Message $message) use ($bot, $telegramBot) {
+            $video = Video::getRandomVideo([]);
+            $answer = $video->original_url;
+            $telegramBot->sendMessage($message->getChat()->getId(), $answer);
+        });
+
+
+        return $bot->run();
     }
 
 }
